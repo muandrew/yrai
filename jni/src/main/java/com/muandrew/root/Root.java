fd9c11e81088ed77ee0703dd8599e5838929fcd4
Andrew Mu <muandrew@gmail.com>
2018-07-25 23:26:09 -0700

    Add example arguments as comment

--- jni/src/main/java/com/muandrew/root/Root.java
+++ jni/src/main/java/com/muandrew/root/Root.java
@@ -6,6 +6,9 @@ import java.awt.image.ColorModel;
 
 class Root {
 
+    // -Djava.library.path="${workspace_loc:project}\lib;${env_var:PATH}"
+    // vm arguments
+    // -Djava.library.path="./cpp/build/lib"
     public static void main(String[] args) {
         ColorModel.getRGBdefault();
         String javaLibPath = System.getProperty("java.library.path");
